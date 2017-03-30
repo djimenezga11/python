ba6d0a739cb39e36a3db6dcc2188e8614ec14946
DW31 <ik012982i9@H012982.NET>
2017-03-30 09:41:19 +0200

    a

---
+++ funciones.py
@@ -0,0 +1,29 @@
+import Datos_Renfe as m
+import Renfe as r
+def ida():
+    print("Has selecionado ida")
+    print(m.zonas)
+    destino = input("Elija su destino:")
+    destino.title()
+    zona = input("Elija su zona:")
+    for z, p in m.zonas.items():
+        for d in p:
+            if d == destino:
+                 zona = z
+            break
+    print(m.precios[r.opc][abs(int(zona)-r.mizona)])
+
+def idavuelta():
+    print(m.zonas) 
+    destino = input("Elija su destino:")
+    destino.title()   
+    zona = input("Elija su zona:")
+    for z, p in m.zonas.items():
+        for d in p:
+            if d == destino:
+                 zona = z
+            break
+    print(m.precios[r.opc][abs(int(zona)-r.mizona)])
+
+def mensual():
+    print("Bono mensual")
